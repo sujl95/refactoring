bde3cdf394ffaaf819e2a044001f04cbe7134450
sujl95 <sujl95@naver.com>
2022-03-20 19:09:14 +0900

    리팩토링 3. 필드 이름 바꾸기

---
+++ src/main/java/me/thewing/refactoring/_01_smell_mysterious_name/_03_rename_field/StudyReview.java
@@ -0,0 +1,4 @@
+package me.thewing.refactoring._01_smell_mysterious_name._03_rename_field;
+
+public record StudyReview(String reviewer, String review) {
+}
---
+++ src/main/java/me/thewing/refactoring/_01_smell_mysterious_name/_03_rename_field/after/StudyDashboard.java
@@ -0,0 +1,41 @@
+package me.thewing.refactoring._01_smell_mysterious_name._03_rename_field.after;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import me.thewing.refactoring._01_smell_mysterious_name._03_rename_field.StudyReview;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+public class StudyDashboard {
+
+    private Set<StudyReview> studyReviews = new HashSet<>();
+
+    /**
+     * 스터디 리뷰 이슈에 작성되어 있는 리뷰어 목록과 리뷰를 읽어옵니다.
+     * @throws IOException
+     */
+    private void loadReviews() throws IOException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(30);
+
+        List<GHIssueComment> reviews = issue.getComments();
+        for (GHIssueComment review : reviews) {
+            studyReviews.add(new StudyReview(review.getUserName(), review.getBody()));
+        }
+    }
+
+    public Set<StudyReview> getStudyReviews() {
+        return studyReviews;
+    }
+
+    public static void main(String[] args) throws IOException {
+        StudyDashboard studyDashboard = new StudyDashboard();
+        studyDashboard.loadReviews();
+        studyDashboard.getStudyReviews().forEach(System.out::println);
+    }
+}
