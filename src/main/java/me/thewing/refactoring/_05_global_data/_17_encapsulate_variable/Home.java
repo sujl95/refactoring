11b20bb8806ff4de29dc1678500e7b1d5c8e2a0e
sujl95 <sujl95@naver.com>
2022-03-26 16:00:09 +0900

    냄새 5. 전역 데이터, 리팩토링 17. 변수 캡슐화하기 After

--- src/main/java/me/thewing/refactoring/_05_global_data/_17_encapsulate_variable/Home.java
+++ src/main/java/me/thewing/refactoring/_05_global_data/_17_encapsulate_variable/Home.java
@@ -3,8 +3,8 @@ package me.thewing.refactoring._05_global_data._17_encapsulate_variable;
 public class Home {
 
     public static void main(String[] args) {
-        System.out.println(Thermostats.targetTemperature);
-        Thermostats.targetTemperature = 68;
-        Thermostats.readInFahrenheit = false;
+        System.out.println(Thermostats.getTargetTemperature());
+        Thermostats.setTargetTemperature(68);
+        Thermostats.setReadInFahrenheit(false);
     }
 }
--- src/main/java/me/thewing/refactoring/_05_global_data/_17_encapsulate_variable/Thermostats.java
+++ src/main/java/me/thewing/refactoring/_05_global_data/_17_encapsulate_variable/Thermostats.java
@@ -2,12 +2,44 @@ package me.thewing.refactoring._05_global_data._17_encapsulate_variable;
 
 public class Thermostats {
 
-    public static Integer targetTemperature = 70;
+    private static Integer targetTemperature = 70;
 
-    public static Boolean heating = true;
+    private static Boolean heating = true;
 
-    public static Boolean cooling = false;
+    private static Boolean cooling = false;
 
-    public static Boolean readInFahrenheit = true;
+    private static Boolean readInFahrenheit = true;
 
+
+    public static Integer getTargetTemperature() {
+        return targetTemperature;
+    }
+
+    public static void setTargetTemperature(Integer targetTemperature) {
+        Thermostats.targetTemperature = targetTemperature;
+    }
+
+    public static Boolean getHeating() {
+        return heating;
+    }
+
+    public static void setHeating(Boolean heating) {
+        Thermostats.heating = heating;
+    }
+
+    public static Boolean getCooling() {
+        return cooling;
+    }
+
+    public static void setCooling(Boolean cooling) {
+        Thermostats.cooling = cooling;
+    }
+
+    public static Boolean getReadInFahrenheit() {
+        return readInFahrenheit;
+    }
+
+    public static void setReadInFahrenheit(Boolean readInFahrenheit) {
+        Thermostats.readInFahrenheit = readInFahrenheit;
+    }
 }
