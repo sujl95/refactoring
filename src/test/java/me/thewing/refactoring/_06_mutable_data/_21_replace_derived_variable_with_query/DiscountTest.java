d91c4f29950f45832d734c27a5caa70486764240
sujl95 <sujl95@naver.com>
2022-04-02 23:29:43 +0900

    냄새 6. 가변 데이터, 리팩토링 21. 파생 변수를 질의 함수로 바꾸기 Before

--- src/test/java/me/thewing/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/DiscountTest.java
+++ src/test/java/me/thewing/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/DiscountTest.java
@@ -1,8 +1,8 @@
 package me.thewing.refactoring._06_mutable_data._21_replace_derived_variable_with_query;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class DiscountTest {
 
