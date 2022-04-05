3297629764f65dc3cee9f8ee8a934c0f5c7f5f26
sujl95 <sujl95@naver.com>
2022-04-05 23:57:16 +0900

    냄새 7. 뒤엉킨 변경, 리팩토링 25. 함수 옮기기 After

--- src/main/java/me/thewing/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
+++ src/main/java/me/thewing/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
@@ -3,13 +3,22 @@ package me.thewing.refactoring._07_divergent_change._24_split_phase;
 public class PriceOrder {
 
     public double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
+        final PriceData priceData = calculatePriceData(product, quantity);
+        return applyShipping(priceData, shippingMethod);
+    }
+
+    private PriceData calculatePriceData(Product product, int quantity) {
         final double basePrice = product.basePrice() * quantity;
         final double discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
-        final double shippingPerCase = (basePrice > shippingMethod.discountThreshold()) ?
+        return new PriceData(basePrice, discount, quantity);
+    }
+
+    private double applyShipping(PriceData priceData, ShippingMethod shippingMethod) {
+        final double shippingPerCase = (priceData.basePrice() > shippingMethod.discountThreshold()) ?
                 shippingMethod.discountedFee() : shippingMethod.feePerCase();
-        final double shippingCost = quantity * shippingPerCase;
-        final double price = basePrice - discount + shippingCost;
+        final double shippingCost = priceData.quantity() * shippingPerCase;
+        final double price = priceData.basePrice() - priceData.discount() + shippingCost;
         return price;
     }
 }
