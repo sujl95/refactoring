035b7cd2f5ef2bbd3902b66b1ec94b1eb65a8d82
sujl95 <sujl95@naver.com>
2022-04-15 12:24:11 +0900

    냄새 13. 반복문 After

--- src/main/java/me/thewing/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
+++ src/main/java/me/thewing/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
@@ -1,7 +1,8 @@
 package me.thewing.refactoring._13_loop._33_replace_loop_with_pipeline;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Author {
 
@@ -14,16 +15,12 @@ public class Author {
         this.twitterHandle = twitterHandle;
     }
 
-    static public List<String> TwitterHandles(List<Author> authors, String company) {
-        var result = new ArrayList<String> ();
-        for (Author a : authors) {
-            if (a.company.equals(company)) {
-                var handle = a.twitterHandle;
-                if (handle != null)
-                    result.add(handle);
-            }
-        }
-        return result;
+    public static List<String> TwitterHandles(List<Author> authors, String company) {
+        return authors.stream()
+            .filter(a -> a.company.equals(company))
+            .map(a -> a.twitterHandle)
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
     }
 
 }
