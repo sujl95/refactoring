79060776df67fe4b6fd59d61631d0aeea9592771
sujl95 <sujl95@naver.com>
2022-04-17 20:23:36 +0900

    냄새 19. 내부자 거래 After

--- src/test/java/me/thewing/refactoring/_19_insider_trading/CheckInTest.java
+++ src/test/java/me/thewing/refactoring/_19_insider_trading/CheckInTest.java
@@ -1,19 +1,18 @@
 package me.thewing.refactoring._19_insider_trading;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class CheckInTest {
 
     @Test
     void isFastPass() {
-        CheckIn checkIn = new CheckIn();
-        assertTrue(checkIn.isFastPass(new Ticket(LocalDate.of(2021, 12, 31), true)));
-        assertFalse(checkIn.isFastPass(new Ticket(LocalDate.of(2021, 12, 31), false)));
-        assertFalse(checkIn.isFastPass(new Ticket(LocalDate.of(2022, 1, 2), true)));
+        assertTrue(new Ticket(LocalDate.of(2021, 12, 31), true).isFastPass());
+        assertFalse(new Ticket(LocalDate.of(2021, 12, 31), false).isFastPass());
+        assertFalse(new Ticket(LocalDate.of(2022, 1, 2), true).isFastPass());
     }
 
 }
