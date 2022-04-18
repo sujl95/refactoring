df91d3a449c9a6f99aff57d14f7be79693f2041e
sujl95 <sujl95@naver.com>
2022-04-18 16:14:11 +0900

    냄새 21. 서로 다른 인터페이스의 대안 클래스들 Before

---
+++ src/main/java/me/thewing/refactoring/_21_alternative_classes_with_different_interfaces/AlertMessage.java
@@ -0,0 +1,12 @@
+package me.thewing.refactoring._21_alternative_classes_with_different_interfaces;
+
+public class AlertMessage {
+
+    public void setMessage(String message) {
+
+    }
+
+    public void setFor(String email) {
+
+    }
+}
---
+++ src/main/java/me/thewing/refactoring/_21_alternative_classes_with_different_interfaces/EmailService.java
@@ -0,0 +1,5 @@
+package me.thewing.refactoring._21_alternative_classes_with_different_interfaces;
+
+public interface EmailService {
+    void sendEmail(EmailMessage emailMessage);
+}
