138d47e5be8fe9a8c0fd87d3945e3589d44ddce2
sujl95 <sujl95@naver.com>
2022-04-21 23:50:22 +0900

    냄새 23. 상속 포기, After

--- src/main/java/me/thewing/refactoring/_23_refused_bequest/Employee.java
+++ src/main/java/me/thewing/refactoring/_23_refused_bequest/Employee.java
@@ -2,10 +2,4 @@ package me.thewing.refactoring._23_refused_bequest;
 
 public class Employee {
 
-    protected Quota quota;
-
-    protected Quota getQuota() {
-        return new Quota();
-    }
-
 }
--- src/main/java/me/thewing/refactoring/_23_refused_bequest/Salesman.java
+++ src/main/java/me/thewing/refactoring/_23_refused_bequest/Salesman.java
@@ -2,5 +2,10 @@ package me.thewing.refactoring._23_refused_bequest;
 
 public class Salesman extends Employee {
 
+  protected Quota quota;
+
+  protected Quota getQuota() {
+      return new Quota();
+  }
 }
 
